eb1603b137fb62397e42de1a0b3b5f362e27df12
Ohm Nabar <12502156+ohm-nabar@users.noreply.github.com>
2020-11-25 22:40:25 -0500

    Most condensed version

--- project_2.lisp
+++ project_2.lisp
@@ -1,4 +1,4 @@
-(definec pow (n :nat count :nat) :nat
+definec pow (n :nat count :nat) :nat
   (if (zp count)
     1
     (* n (pow n (1- count)))))
@@ -11,7 +11,7 @@
     1
     (* n (fact (1- n)))))
 
-(defthm pow-pow-1 (implies (and (natp m) (> m 0) (natp n)) (<= (pow (- m 1) n)  (pow m n))))
+(defthm pow-pow-1 (implies (and (natp n) (> n 0) (natp m)) (<= (pow (- n 1) m)  (pow n m))))
 
 (defthm <=-trans (implies (and (natp a)
                                (natp b)
@@ -20,12 +20,6 @@
                                (<= b c))
                           (<= a c)))
 
-; Additonal suggested follow-up induction step 1.2
-(defthm add-pow-n (implies (and (natp n)
-                                (> n 0)
-                                (<= (fact (+ -1 n)) (pow (+ -1 n) (+ -1 n))))
-                           (<= (fact (+ -1 n)) (pow n (+ -1 n))))
-  :hints (("Goal''" :use (:instance <=-trans (a (fact (+ -1 n))) (b (pow (+ -1 n) (+ -1 n))) (c (pow n (+ -1 n)))))))
-
 ; MAIN
-(thm (implies (natp n) (<= (fact n) (n-to-n n))))
+(thm (implies (natp n) (<= (fact n) (n-to-n n)))
+      :hints (("Subgoal *1/4''" :use (:instance <=-trans (a (fact (+ -1 n))) (b (pow (+ -1 n) (+ -1 n))) (c (pow n (+ -1 n)))))))
