4e74cf5eae2f392cb28514ad994ad09d8b7bbfc1
Ohm Nabar <12502156+ohm-nabar@users.noreply.github.com>
2020-11-29 19:07:18 -0500

    Update project_2.lisp
    Fully documented

--- project_2.lisp
+++ project_2.lisp
@@ -1,18 +1,26 @@
-definec pow (n :nat count :nat) :nat
+; Ohm Nabar and Robert Sanders
+
+; pow- multiply n to itself count times (when count is 0, the result is 1)
+(definec pow (n :nat count :nat) :nat
   (if (zp count)
     1
     (* n (pow n (1- count)))))
 
+; n-to-n- multiply n to itself n times
 (definec n-to-n (n :nat) :nat
   (pow n n))
 
+; fact- the multiplication of {n, n-1, ... 1} (when n is 0, the result is 1)
 (definec fact (n :nat) :nat
   (if (zp n)
     1
     (* n (fact (1- n)))))
 
-(defthm pow-pow-1 (implies (and (natp n) (> n 0) (natp m)) (<= (pow (- n 1) m)  (pow n m))))
+; with the power unchanged, applying pow with base (1- n) always results in an answer
+; less than or equal to the answer produced by applying pow with base n
+(defthm pow-pow-base (implies (and (natp n) (> n 0) (natp m)) (<= (pow (1- n) m)  (pow n m))))
 
+; the <= operator is transitive over the natural numbers
 (defthm <=-trans (implies (and (natp a)
                                (natp b)
                                (natp c)
@@ -20,6 +28,6 @@ definec pow (n :nat count :nat) :nat
                                (<= b c))
                           (<= a c)))
 
-; MAIN
-(thm (implies (natp n) (<= (fact n) (n-to-n n)))
-      :hints (("Subgoal *1/4''" :use (:instance <=-trans (a (fact (+ -1 n))) (b (pow (+ -1 n) (+ -1 n))) (c (pow n (+ -1 n)))))))
+; our main theorem- the factorial of n is less than or equal to n^n for all natural numbers
+(defthm fact-n-to-n (implies (natp n) (<= (fact n) (n-to-n n)))
+     :hints (("Subgoal *1/4''" :use (:instance <=-trans (a (fact (+ -1 n))) (b (pow (+ -1 n) (+ -1 n))) (c (pow n (+ -1 n)))))))
